9f683acb360ea59821baed4296727c741ba5648f
Eoin Lernihan <eoinblernihan@gmail.com>
2021-04-25 04:18:11 +0100

    finding a word in a file

--- projectCode.py
+++ projectCode.py
@@ -1,14 +1,23 @@
 path = 'c:\\Users\\eoinb\\Desktop\\graphy thory\\graph-theory\\test.txt'
 f = open('test.txt', 'w')
-f.write('cola\n')
-f.write('fanta\n')
+f.write('cocacola\n')
+f.write('fanta and culb orange\n')
 f.write('z-up\n')
 f.close()
 f = open('test.txt', 'r')
 line = f.readline() 
 while line:
+        
+        if "culb" in line:
+                print("query found")
         line = line.rstrip()  # strip trailing spaces and newline
         # process the line
         print(line)
         line = f.readline()
+f.close()
+f = open('test.txt', 'r')
+line = f.readline() 
+
+if "culb" in line:
+    print("query found")
 f.close()
