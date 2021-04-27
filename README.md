32f63cd424dec1ae68907ef59ac3758b2114877a
Eoin Lernihan <eoinblernihan@gmail.com>
2021-04-27 02:39:17 +0100

    cleand up code + made function to keep homemade re

--- README.md
+++ README.md
@@ -3,16 +3,20 @@
 
 
 ## description 
-
+    A simple search for txt that can print, write and find words in lines amongst them
 
 ## Instructions
+    Type in python project.py on the command line
+    type in the file directory
+    select if you want it to find a word
+    and let the code do the rest
 
 ## The explanation of the algorithm
 
 
 ## What is a regular expression?
-
-
+regex is a string of text that allows you to create patterns that help match, locate, and manage text
+A regular expression (regex or regexp for short) is a special text string for describing a search pattern. You can think of regular expressions as wildcards on steroids.(https://www.regular-expressions.info/)
 
 ## How do regular expressions differ across implementations?
 
--- projectCode.py
+++ projectCode.py
@@ -1,18 +1,44 @@
-path = input("Enter the direct  ")
-word = input("Enter the word you would like to found ")
-f = open('test.txt', 'w')
-f.write('cocacola\n')
-f.write('fanta and culb orange\n')
-f.write('z-up\n')
-f.close()
-f = open('test.txt', 'r')
-line = f.readline() 
-while line:
-        
+def prams():
+ word = input("Enter the word you would like to found ")
+
+
+
+print("Hello there user")
+word=''
+i = 0
+i = float(input("Would you like to use this program today"))
+while i > 1:
+  
+  path = input("Enter the directory you would like to use ")
+  prams()
+  
+  f = open(path, 'w')
+  f.write('cocacola\n')
+  f.write('fanta and culb orange\n')
+  f.write('z-up\n')
+  f.close()
+  f = open(path, 'r')
+  line = f.readline() 
+  while line:   
         if word in line:
                 print("query found")
-        line = line.rstrip()  # strip trailing spaces and newline
+        line = line.rstrip() 
+        # strip trailing spaces and newline
         # process the line
         print(line)
+      
         line = f.readline()
-f.close()
+  f.close()
+  i = input("would you like to contineu ")
+
+print("Thank you for using this code:)")
+print("Have a wondefulday")
+
+
+
+
+
+
+
+
+
