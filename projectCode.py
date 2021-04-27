d937dcaf2a253f1b63e330c210df909bf9859465
Eoin Lernihan <eoinblernihan@gmail.com>
2021-04-27 14:39:44 +0100

    draft for ^$. regular expersion

--- projectCode.py
+++ projectCode.py
@@ -1,12 +1,31 @@
 def prams():
+ 
  word = input("Enter the word you would like to found ")
+ checker = word
 
+ endsWithMatch = checker.endswith('$') 
+ startsWithMatch =   checker.startswith('^')
+ exactMatch = startsWithMatch and endsWithMatch
+
+ if '.' in word :
+    print("query found")
+ if exactMatch == True :
+    print (checker.endswith('$'))
+    print (checker.startswith('^'))
+    trueWord = word.replace("$", "")
+ elif startsWithMatch == True :
+    print (checker.startswith('^'))
+ elif startsWithMatch == True :
+    print (checker.endswith('$'))
+
+
+ 
 
 
 print("Hello there user")
 word=''
 i = 0
-i = float(input("Would you like to use this program today"))
+i = float(input("Would you like to use this program today(yes is a number greater than 1)"))
 while i > 1:
   
   path = input("Enter the directory you would like to use ")
@@ -29,7 +48,7 @@ while i > 1:
       
         line = f.readline()
   f.close()
-  i = input("would you like to contineu ")
+  i = float(input("would you like to contineu "))
 
 print("Thank you for using this code:)")
 print("Have a wondefulday")
