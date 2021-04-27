3866479d363b7b11442d4d195af802e553bf802c
Eoin Lernihan <eoinblernihan@gmail.com>
2021-04-27 15:45:22 +0100

    Add alteritives and fixed formating

--- projectCode.py
+++ projectCode.py
@@ -1,63 +1,49 @@
-def prams():
- 
- word = input("Enter the word you would like to found ")
- checker = word
-
- endsWithMatch = checker.endswith('$') 
- startsWithMatch =   checker.startswith('^')
- exactMatch = startsWithMatch and endsWithMatch
-
- if '.' in word :
-    print("query found")
- if exactMatch == True :
-    print (checker.endswith('$'))
-    print (checker.startswith('^'))
-    trueWord = word.replace("$", "")
- elif startsWithMatch == True :
-    print (checker.startswith('^'))
- elif startsWithMatch == True :
-    print (checker.endswith('$'))
-
-
- 
-
-
 print("Hello there user")
-word=''
-i = 0
-i = float(input("Would you like to use this program today(yes is a number greater than 1)"))
-while i > 1:
-  
-  path = input("Enter the directory you would like to use ")
-  prams()
-  
-  f = open(path, 'w')
-  f.write('cocacola\n')
-  f.write('fanta and culb orange\n')
-  f.write('z-up\n')
-  f.close()
-  f = open(path, 'r')
-  line = f.readline() 
-  while line:   
-        if word in line:
-                print("query found")
-        line = line.rstrip() 
-        # strip trailing spaces and newline
-        # process the line
-        print(line)
-      
-        line = f.readline()
-  f.close()
-  i = float(input("would you like to contineu "))
+word = ''
+
+path = input("Enter the directory you would like to use ")
+
+word = input("Enter the regular expertion you would like to found ")
+checker = word
+endsWithMatch = checker.endswith('$')
+startsWithMatch = checker.startswith('^')
+alternatives = "|" in checker
+trueWords = checker.split("|")
+exactMatch = startsWithMatch and endsWithMatch
+print(exactMatch)
+trueWord = word
+if '.' in word:
+  print("query found")
+if startsWithMatch == True:
+  trueWord = trueWord.replace('^', '')
+if endsWithMatch == True:
+  trueWord = trueWord.replace('$', '')
+f = open(path, 'r')
+line = f.readline()
+while line:
+  if exactMatch:
+    if trueWord == line:
+      print("exactMatch found")
+      line = line.rstrip()
+  elif startsWithMatch:
+    if line.startswith(trueWord):
+      print("query found")
+      line = line.rstrip()
+  elif endsWithMatch:
+    if line.endswith(trueWord):
+      print("query found")
+      line = line.rstrip()
+  elif alternatives:
+    if any(substring in line for substring in trueWords):
+      print("alternatives query found")
+      line = line.rstrip()
+  else:
+    if trueWord in line:
+      print("query found")
+      line = line.rstrip()
+      print(line)
+  line = f.readline()
+f.close()
 
 print("Thank you for using this code:)")
 print("Have a wondefulday")
-
-
-
-
-
-
-
-
-
